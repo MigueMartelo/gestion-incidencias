49a48f164815ba0ccb8185e757fc088926520c73
MigueMartelo <miguel@utbvirtual.edu.co>
2017-07-12 16:22:24 -0500

    First commit

---
+++ resources/views/includes/menu.blade.php
@@ -0,0 +1,21 @@
+<div class="panel panel-primary">
+    <div class="panel-heading">Menú</div>
+
+    <div class="panel-body">
+        <div class="list-group">
+		  <a href="#" class="list-group-item">
+		    Dashboard
+		  </a>
+		  <a href="#" class="list-group-item">
+		  	Ver incidencias
+		  </a>
+		  <a href="#" class="list-group-item">
+		  	Reportar incidencia
+		  </a>
+		  <a href="#" class="list-group-item">
+		  	Administración
+		  </a>
+		</div>
+    </div>
+</div>
+
