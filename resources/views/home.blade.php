9689a55a766d624cf5888100a16dd9c2cd08340d
MigueMartelo <miguel@utbvirtual.edu.co>
2017-07-13 10:57:59 -0500

    Modeling using migrations

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="panel-heading">Dashboard</div>
 
     <div class="panel-body">
-        You are logged in!
+        Hola <strong>{{ Auth::User()->name }}</strong>
     </div>
 </div>
 @endsection
